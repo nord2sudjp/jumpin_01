83bf3f393707ffdc5f9c961a705ce173cb47f979
maka y <nord2sudjp@gmail.com>
2021-02-16 22:09:30 +0900

    connected db

--- lib/widgets/montly_header.dart
+++ lib/widgets/montly_header.dart
@@ -17,13 +17,35 @@ class MonthlyHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //print(mastermonthly.toString());
-    final YearMonthsData = Provider.of<YearMonths>(context, listen: false);
-    final yearmonths = YearMonthsData.items;
+    //final YearMonthsData = Provider.of<YearMonths>(context, listen: false);
+    //final yearmonths = YearMonthsData.items;
 
-    return ListView.builder(
-        itemBuilder: (ctx, index) {
-          return YearMonthCard(yearmonths[index].year, yearmonths[index].month);
-        },
-        itemCount: yearmonths.length);
+    return FutureBuilder(
+      future: Provider.of<YearMonths>(context, listen: false).fetchYearMonth(),
+      builder: (ctx, dataSnapshot) {
+        if (dataSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if (dataSnapshot.error != null) {
+            // ...
+            // Do error handling stuff
+            return Center(
+              child: Text('An error occurred!'),
+            );
+          } else {
+            return Consumer<YearMonths>(builder: (ctx, yearmonthsData, child) {
+              print("Consumer Notified");
+              print(yearmonthsData.items.length);
+              return ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return YearMonthCard(yearmonthsData.items[index].year,
+                        yearmonthsData.items[index].month);
+                  },
+                  itemCount: yearmonthsData.items.length);
+            });
+          }
+        }
+      },
+    );
   }
 }
