5df70a3d484ce8d2711ef9dc35b1a4862bd8aca0
n2sjp <nord2sudjp@gmail.com>
2021-09-25 23:07:21 +0900

    commit

--- lib/providers/monthly_routes.dart
+++ lib/providers/monthly_routes.dart
@@ -15,11 +15,11 @@ class MonthlyRoutes with ChangeNotifier {
 
       final databaseReference = FirebaseDatabase.instance.reference();
       final db = databaseReference.child("monthly_route");
-      print("fetchMonthlyRoute:" + "Ref created");
+      //print("fetchMonthlyRoute:" + "Ref created");
 
       Map<dynamic, dynamic> extractedData;
       DataSnapshot snapshot = await db.once();
-      print("fetchMonthlyRoute:" + "Once completd");
+      //print("fetchMonthlyRoute:" + "Once completd");
 
       extractedData = snapshot.value;
       // db.once().then((DataSnapshot snapshot) {
@@ -36,14 +36,15 @@ class MonthlyRoutes with ChangeNotifier {
             month: data['month'],
             id: data['id'],
             grade: data['grade'],
-            creator: data['creator']));
+            creator: data['creator'],
+            topo: data['topo']));
         //  MonthlyRoute(year: 2020, month: 11, id: 1, grade: 4, creator: 'Maka'),
       });
       _items = loadedMonthlyRoute;
-      print("fetchMonthlyRoute" + "Data loaded");
+      //print("fetchMonthlyRoute" + "Data loaded");
       notifyListeners();
     } catch (error) {
-      print("fetchMonthlyRoute" + error.toString());
+      //print("fetchMonthlyRoute" + error.toString());
       //throw (error);
     }
     ;
